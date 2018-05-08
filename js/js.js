250c25555619ea86f90774428ef5277f5484287e
Dave Hoffman <spaghettidave80@gmail.com>
2018-05-07 21:06:46 -0400

    Final product

--- js/js.js
+++ js/js.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+  // Hamburger function
   $('.hamburger').on('click', function(){
     $('.hamburger').toggleClass('active');
     $('.drop-down').toggleClass('active');
   });
 
+  // Nav link click function
   $('.nav-link').on('click', function(){
     let navHeight = $('.navbar').outerHeight();
     $('.hamburger').toggleClass('active');
@@ -18,5 +20,22 @@ $(document).ready(function(){
   $('.drop-down, .drop-down-menu, .menu-items').on('click', function(){
     $('.hamburger').toggleClass('active');
     $('.drop-down').toggleClass('active');
+  });
+
+  // Nav fade in and out
+  $('.navbar').hide();
+
+  let topOfAboutDiv = $('.about').offset().top;
+  
+  $(window).resize(function(){
+    topOfAboutDiv = $('.about').offset().top;
+  });
+  
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= (topOfAboutDiv - 50)){
+      $('.navbar').fadeIn('slow');
+    } else {
+      $('.navbar').fadeOut('slow');
+    }
   });
 });
